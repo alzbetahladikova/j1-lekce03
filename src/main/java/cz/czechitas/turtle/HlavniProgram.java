514f6e32aae9ac96d384b9dd6777686465929c0c
alzbetahladikova <beta.strelbova@gmail.com>
2023-02-21 20:35:07 +0100

    přidání parametru

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,12 +2,29 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka;
 
     public void start() {
         zofka = new Turtle();
+
+        nakresliRovnoramennyTrojuhelnik (200,200, 100,Color.MAGENTA);
+
     }
+    private void nakresliRovnoramennyTrojuhelnik(double x, double y, double delkaStrany, Color barva) {
+        zofka.setLocation(x, y);
+        zofka.setPenColor(barva);
+        for (int i = 0; i < 3; i++) {
+            zofka.move (delkaStrany);
+            zofka.turnLeft(120);
+        }
+    }
+
+
+
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
